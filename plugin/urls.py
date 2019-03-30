3ece540dc7fcca5ad2dce0a2de55afea1ed391b6
Daniel sepulveda <daniel.sendmemail@gmail.com>
2019-03-30 15:09:30 -0500

    urls simplified

--- plugin/urls.py
+++ plugin/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from plugin.views import ShowScenarioView
 
 urlpatterns = [
-    path('plugin/demo', ShowScenarioView.as_view(), name='show-plugin-scenario'),
+    path('', ShowScenarioView.as_view(), name='show-plugin-scenario'),
 
 ]
